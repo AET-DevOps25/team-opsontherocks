bc50d4f47a45394187ba30e530eba492b757425f
Gamila Dorgham <gamila.dorgham@tum.de>
2025-05-12 01:03:05 +0200

    implement Springboot WheelOfLifeApplication

---
+++ server/wheel-of-life/src/main/java/com/opsontherocks/wheel_of_life/WheelOfLifeApplication.java
@@ -0,0 +1,13 @@
+package com.opsontherocks.wheel_of_life;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheelOfLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WheelOfLifeApplication.class, args);
+	}
+
+}
---
+++ server/wheel-of-life/src/test/java/com/opsontherocks/wheel_of_life/WheelOfLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opsontherocks.wheel_of_life;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheelOfLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
