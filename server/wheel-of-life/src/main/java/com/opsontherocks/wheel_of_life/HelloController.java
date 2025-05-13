595f7848c966bfd97fee5ce586efb3c41c506275
Simon Paul <simon.paul@tum.de>
2025-05-13 02:21:13 +0200

    added hellocontroller to spring boot

---
+++ server/wheel-of-life/src/main/java/com/opsontherocks/wheel_of_life/HelloController.java
@@ -0,0 +1,13 @@
+package com.opsontherocks.wheel_of_life;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, Wheel of Life!";
+    }
+}
