e58688343b4388e3596c1b41135fa5d7b8db3db9
Simon Paul <simon.paul@tum.de>
2025-05-18 03:24:15 +0200

    fixed cors error on server by environment variables

--- server/wheel-of-life/src/main/java/com/opsontherocks/wheel_of_life/WebConfig.java
+++ server/wheel-of-life/src/main/java/com/opsontherocks/wheel_of_life/WebConfig.java
@@ -6,12 +6,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    private final String clientOrigin =
+            System.getenv().getOrDefault("CLIENT_ORIGIN", "http://localhost:5173");
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry
-                .addMapping("/**")                         // apply to all endpoints
-                .allowedOrigins("http://localhost:5173")   // your front-end origin
-                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+                .addMapping("/**")
+                .allowedOrigins(clientOrigin)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }
 }
