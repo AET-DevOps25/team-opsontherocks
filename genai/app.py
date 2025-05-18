e1ec91feac47a4519ef4969fb83119ae531e2dd5
Simon Paul <simon.paul@tum.de>
2025-05-18 03:20:53 +0200

    fixed cors error on genai microservice by environment variable

--- genai/app.py
+++ genai/app.py
@@ -1,8 +1,11 @@
+import os
 from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
+
+client_origin = os.getenv('CLIENT_ORIGIN', 'http://localhost:5173')
+CORS(app, resources={r"/*": {"origins": client_origin}})
 
 @app.route('/hello', methods=['GET'])
 def hello_python():
